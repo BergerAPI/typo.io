c1ba7cd6df21da5da7d5b48e711c1ba34646a945
niclas <bergerbtw@gmail.com>
2021-04-05 17:50:34 +0200

    added origin to cors

--- pages/api/language/[language].js
+++ pages/api/language/[language].js
@@ -5,6 +5,7 @@ import Cors from 'cors'
 // Initializing the cors middleware
 const cors = Cors({
   methods: ['GET', 'HEAD'],
+  origin: "*"
 })
 
 // Helper method to wait for a middleware to execute before continuing
