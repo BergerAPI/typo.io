aa0119a76d6c8686737cd17b9e4cd5539ed91f78
niclas <bergerbtw@gmail.com>
2021-04-06 21:45:59 +0200

    added raw to the logic

--- util/logic/type-logic.js
+++ util/logic/type-logic.js
@@ -1,7 +1,17 @@
 
+
+/**
+ * Calculates the wpm and the accuracy
+ * @param {number} time 
+ * @param {string} typedText 
+ * @param {number} errors 
+ * @param {Array} words 
+ * @returns 
+ */
 export function calculate(time, typedText, errors, words) {
     const mins = Math.round((time / 1000) * 1) / 1
-    let wpm = Math.round(((typedText.length - errors + words.length - 2) * (60 / mins)) / 5)
+    let wpm = Math.round(((typedText.length - errors + (words.length - 2)) * (60 / mins)) / 5)
+    let raw = Math.round((typedText.length * (60 / mins)) / 5);
 
     const rawAccuracy = (((typedText.length - errors) / typedText.length) * 100)
     let accuracy = Math.round(rawAccuracy)
@@ -12,10 +22,14 @@ export function calculate(time, typedText, errors, words) {
     if (isNaN(wpm) || wpm.toString().includes("-Infinity"))
         wpm = 0
 
+    if (isNaN(raw) || raw.toString().includes("-Infinity"))
+        raw = 0
+
     if (typedText.length < errors) {
         wpm = 0
         accuracy = 0
+        raw = 0
     }
 
-    return { wpm, accuracy }
+    return { wpm, accuracy, raw }
 }
