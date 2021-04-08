ce9f824df81db4f3f7041c5f9653f021dbaa01fb
niclas <bergerbtw@gmail.com>
2021-04-08 22:56:20 +0200

    fixed analytics

--- util/firebase/firebase.js
+++ util/firebase/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase'
 import 'firebase/database'
+import'firebase/analytics'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
@@ -9,7 +10,8 @@ if (!firebase.apps.length) {
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
         storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
         messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_APP_ID
+        appId: process.env.NEXT_PUBLIC_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
     })
     firebase.analytics()
 }
