0a1aae1d7c6ba2a17ebf786ed5b30dfa787fbd2b
niclas <bergerbtw@gmail.com>
2021-04-11 00:15:22 +0200

    added debugging

--- pages/app/index.js
+++ pages/app/index.js
@@ -9,19 +9,23 @@ export default function App() {
       <div>
         <Input text="Loading..." author="Loading..." finish={(finishState) => {
           setState({ isFinished: true, wpm: finishState.wpm, raw: finishState.raw, accuracy: finishState.accuracy, text: finishState.text, author: finishState.author })
+          console.log("finished")
         }} />
       </div>
     )
-  else return (
-    <div>
-      <h1>WPM: {state.wpm}</h1>
-      <h1>Raw: {state.raw}</h1>
-      <h1>Accuracy: {state.accuracy}</h1>
-      <h1>Author: {state.author}</h1>
+  else {
+    console.log("rendering")
+    return (
+      <div>
+        <h1>WPM: {state.wpm}</h1>
+        <h1>Raw: {state.raw}</h1>
+        <h1>Accuracy: {state.accuracy}</h1>
+        <h1>Author: {state.author}</h1>
 
-      <button onClick={() => {
-        window.location.reload()
-      }}>Next Game</button>
-    </div>
-  )
+        <button onClick={() => {
+          window.location.reload()
+        }}>Next Game</button>
+      </div>
+    )
+  }
 }
