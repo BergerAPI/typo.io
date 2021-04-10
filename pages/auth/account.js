21fac794db35d0869035f9ea0058f5eda024b43e
niclas <bergerbtw@gmail.com>
2021-04-10 14:28:34 +0200

    fixed username check

--- pages/auth/account.js
+++ pages/auth/account.js
@@ -44,7 +44,7 @@ export default function Account() {
                     let username = document.querySelector("input[name='username']").value
 
                     await auth.onAuthStateChanged(async (authUser) => {
-                        if (authUser !== null && username.length >= 3 && username.length <= 12 && username.match(/^[A-Za-z]\\w{5,29}$/)) {
+                        if (authUser !== null && username.length >= 3 && username.length <= 12 && username.match(/^[a-zA-Z0-9]+$/)) {
                             await authUser.updateProfile({
                                 displayName: username
                             })
