bac18a9dc57d924a290c28493999ccc5cdb2e4c9
niclas <bergerbtw@gmail.com>
2021-04-10 13:47:23 +0200

    made names changeable

--- pages/auth/account.js
+++ pages/auth/account.js
@@ -17,25 +17,48 @@ export default function Account() {
                 })
             }} type="submit">Logout</button>
 
-            <input type="text" name="link" placeholder="Link" />
-            <button onClick={async () => {
-                let link = document.querySelector("input[name='link']").value
+            <div>
+                <input type="text" name="link" placeholder="Link" />
+                <button onClick={async () => {
+                    let link = document.querySelector("input[name='link']").value
 
-                await auth.onAuthStateChanged(async (authUser) => {
-                    if (authUser !== null) {
-                        await authUser.updateProfile({
-                            photoURL: link
-                        })
+                    await auth.onAuthStateChanged(async (authUser) => {
+                        if (authUser !== null) {
+                            await authUser.updateProfile({
+                                photoURL: link
+                            })
 
-                        const snapshot = await db.collection("stats").get();
+                            const snapshot = await db.collection("stats").get();
 
-                        snapshot.docs.forEach((doc) => {
-                            if (doc.data().userUid === authUser.uid)
-                                db.collection("stats").doc(doc.id).update({ photo: link })
-                        });
-                    }
-                });
-            }} type="submit">Save</button>
+                            snapshot.docs.forEach((doc) => {
+                                if (doc.data().userUid === authUser.uid)
+                                    db.collection("stats").doc(doc.id).update({ photo: link })
+                            });
+                        }
+                    });
+                }} type="submit">Save</button>
+            </div>
+            <div>
+                <input type="text" name="username" placeholder="Username" />
+                <button onClick={async () => {
+                    let username = document.querySelector("input[name='username']").value
+
+                    await auth.onAuthStateChanged(async (authUser) => {
+                        if (authUser !== null) {
+                            await authUser.updateProfile({
+                                displayName: username
+                            })
+
+                            const snapshot = await db.collection("stats").get();
+
+                            snapshot.docs.forEach((doc) => {
+                                if (doc.data().userUid === authUser.uid)
+                                    db.collection("stats").doc(doc.id).update({ displayName: username })
+                            });
+                        }
+                    });
+                }} type="submit">Save</button>
+            </div>
         </>
     )
 }
