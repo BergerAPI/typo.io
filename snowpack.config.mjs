8d9dd6c2995667da56fb26723cfedc8eec33a9b9
niclas <bergerbtw@gmail.com>
2021-05-28 15:54:58 +0200

    rebased everything in svelte

---
+++ snowpack.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {},
+  plugins: [
+    '@snowpack/plugin-svelte'
+  ],
+  routes: [],
+  optimize: {},
+  packageOptions: {},
+  devOptions: {},
+  buildOptions: {},
+};
