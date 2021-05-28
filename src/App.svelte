c86023a613d6d4e7f98daf8c33ff472bb22f181a
peu77 <emilebert@icloud.com>
2021-05-28 17:35:23 +0200

    add basic nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,29 @@
     import Router, {location} from "svelte-spa-router"
     import Nav from "./components/Nav.component.svelte"
     import App from "./pages/App.page.svelte"
+    import User from "./pages/User.page.svelte"
+
+    const navConfig = [
+        {
+            iconPath: "public/ico/game.svg",
+            object: App,
+            path: "/"
+        },
+        {
+            iconPath: "public/ico/user.svg",
+            object: User,
+            path: "/"
+        },
+    ]
+
+    const routerObject = {}
+    navConfig.forEach(route => {
+        routerObject[route.path] = route.object
+    })
+
 </script>
 
-<Router routes={{
-    "/": App
-}}/>
+<Nav {navConfig}/>
 
-<Nav/>
+<Router routes={routerObject}/>
+
--- src/components/Nav.component.svelte
+++ src/components/Nav.component.svelte
@@ -1,10 +1,46 @@
 <script>
 
+    export let navConfig
 
 </script>
 
-<img src="public/ico/user.svg" alt="Usericon"/>
+<nav class="flex">
+    <div class="flex">
+        <h2 id="logo">Typo.io</h2>
+
+        {#each navConfig as route}
+            <div class="route flex">
+                <img src={route.iconPath} alt="routeIcon"/>
+            </div>
+        {/each}
+    </div>
+
+    <div class="fastSettings">
+
+    </div>
+
+</nav>
+
 
 <style>
+    nav {
+        max-width: 500px;
+        margin: 0 auto;
+        justify-content: space-around;
+    }
+
+    .route{
+
+    }
+
+    #logo {
+        margin-right: 10px;
+        color: dodgerblue;
+    }
+
+    .flex {
+        display: flex;
+        align-items: center;
+    }
 
 </style>
